ab6b13e450e69c87a167450413fddf39cf299856
sidelkins <sidelkins77@gmail.com>
2022-06-28 21:43:02 -0500

    fixed whitespace at bottom of page, removed a couple files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import React from 'react';
 import Layout from "./Layout";
-import Home from "./pages/Home";
-import About from "./pages/About"
-import Projects from "./pages/Projects";
 
 function App() {
 
   return (
-    <Layout></Layout>
+      <Layout />
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import React from 'react';
 import * as ReactDOM from 'react-dom'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { BrowserRouter, HashRouter, Routes, Route } from 'react-router-dom';
+import './styles/index.css'
 import App from './App';
 import Home from './pages/Home'
 import About from './pages/About'
 import Projects from './pages/Projects'
-import Layout from './Layout';
+import Resources from './pages/Resources'
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<App/>}>
-        <Route index element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-        </Route>
-      </Routes>
-      </HashRouter>
+      <ColorModeScript initialColorMode='dark' />
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<App/>}>
+            <Route index element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/resources" element={<Resources/>} />
+            </Route>
+          </Routes>
+        </HashRouter>
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
