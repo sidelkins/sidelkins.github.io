01d4d3fdfe868b926e0d178774ee1ac956ba0be0
sidelkins <sidelkins77@gmail.com>
2022-06-30 22:36:44 -0500

    threeJS

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,19 +4,22 @@ import { VStack,
  } from "@chakra-ui/layout";
  import "../styles/animations.css"
  import { Container } from "@chakra-ui/layout";
+import DisplayModel from "../components/DisplayModel";
+import { Scene } from "three";
 
 function Home() {
     return(
-    <VStack boxSize="2xl">
-        <HStack>
-        {/* <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-prompt" height="82" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-        <polyline points="5 7 10 12 5 17" />
-        <line x1="13" y1="17" x2="19" y2="17" />
-        </svg> */}
-        <Heading mt="8" fontSize={64} className="typing-effect">welcome...</Heading>
-        </HStack>
-    </VStack>
+            <DisplayModel></DisplayModel>
+   // <VStack boxSize="2xl">
+    //     <HStack>
+    //     {/* <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-prompt" height="82" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    //     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+    //     <polyline points="5 7 10 12 5 17" />
+    //     <line x1="13" y1="17" x2="19" y2="17" />
+    //     </svg> */}
+    //     <Heading mt="8" fontSize={64} className="typing-effect">welcome...</Heading>
+    //     </HStack>
+    // </VStack>
     );
 }
 
