45f30225b749781b1c5fd19b88aea5d1c213790c
Sid Elkins <sid.elkins@motion.com>
2022-07-28 10:58:54 -0500

    scripts work?

--- gh-pages.cjs
+++ gh-pages.cjs
@@ -3,7 +3,7 @@ var ghpages = require('gh-pages');
 ghpages.publish(
     'public', // path to public directory
     {
-        branch: 'gh-pages',
+        branch: 'svelte',
         repo: 'https://github.com/sidelkins/sidelkins.github.io.git', // Update to point to your repository  
         user: {
             name: 'Sid Elkins', // update to use your name
