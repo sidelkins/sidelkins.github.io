4cec8bed6db2903c5888a45b7a80b58358ab0d88
Sid Elkins <sid.elkins@motion.com>
2022-07-28 11:45:03 -0500

    some changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,7 @@
 <canvas id="banner"></canvas>
-<div class="main-content" style="text-align: center; width: 100%; position: absolute">
-    <center>
-
-      <div class="" style="height: 750px;">
-        <h1 style="padding-top: 230px; color: white;">Hi, my name is _____</h1>
+<div class="main-content" style="text-align: center; width: 100%; position: absolute;">
+      <div style="padding-top: 30px;">
+        <h1 style="padding-top: 230px; color: white; text-shadow: 4px 4px black;">sid elkins</h1>
         <p style="padding-top: 25px; color: white;" id="app"></p>
       </div>
 
@@ -12,11 +10,11 @@
       <!-- Introduction -->
       <div class="flex-container-reverse" style="padding-bottom: 50px; padding-top: 20px;">
         <div class="big-container-left" style="">
-          <p class="time-text">Fun fact: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-          <p style="padding-bottom: 5px;">Hi, my name is ____. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nunc lobortis mattis aliquam faucibus. Faucibus purus in massa tempor nec. Tincidunt praesent semper feugiat nibh. Suscipit tellus mauris a diam maecenas sed enim ut.</p>
+          <p class="time-text"></p>
+          <p style="padding-bottom: 5px;"></p>
         </div>
         <div class="small-container-right">
-          <img src="assets/stockphoto.jpg" class="title-image">
+          <img src="src\assets\placeholder-image.png" class="title-image">
         </div>
       </div>
 
@@ -31,7 +29,7 @@
           <button style="padding-bottom: 5px;" type="button" class="btn btn-outline-success" id="btn1">See more</button>
         </div>
         <div class="small-container-right">
-          <img src="assets/desk setup.webp" class="title-image">
+          <img src="src\assets\placeholder-image.png" class="title-image">
         </div>
       </div>
 
@@ -44,7 +42,7 @@
           <button style="padding-bottom: 5px;" type="button" class="btn btn-outline-success" id="btn2">See more</button>
         </div>
         <div class="small-container-right">
-          <img src="assets/app.webp" class="title-image">
+          <img src="src\assets\placeholder-image.png" class="title-image">
         </div>
       </div>
 
@@ -57,11 +55,69 @@
           <button style="padding-bottom: 5px;" type="button" class="btn btn-outline-success" id="btn3">Visit</button>
         </div>
         <div class="small-container-right">
-          <img src="assets/project3.jpg" class="title-image">
+          <img src="src\assets\placeholder-image.png" class="title-image">
+        </div>
+      </div>
+
+      <h2 style="padding-bottom: 20px">Experience</h2>
+
+      <!-- Experience #3 #1 -->
+      <div class="flex-container-reverse" style="padding-bottom: 50px; padding-top: 40px;">
+        <div class="big-container-left">
+          <h3>Porttitor massa</h3>
+          <p class="time-text">May 2020 - TBD</p> <!--Add your own dates-->
+          <p>Enim praesent elementum facilisis leo vel fringilla est ullamcorper eget. Velit laoreet id donec ultrices. Libero volutpat sed cras ornare. Porttitor leo a diam sollicitudin tempor id eu. Laoreet suspendisse interdum consectetur libero id faucibus nisl tincidunt. Diam vel quam elementum pulvinar etiam. Porttitor rhoncus dolor purus non enim. Quam id leo in vitae. </p>
+          <button style="padding-bottom: 5px;" type="button" class="btn btn-outline-success" id="btn1">See more</button>
+        </div>
+        <div class="small-container-right">
+          <img src="src\assets\placeholder-image.png" class="title-image">
+        </div>
+      </div>
+
+      <!-- Experience #2 -->
+      <div class="flex-container-reverse" style="padding-bottom: 50px; padding-top: 40px;">
+        <div class="big-container-left">
+          <h3>Auctor augue</h3>
+          <p class="time-text">April 2019 - TBD</p> <!--Add your own dates-->
+          <p>Nulla aliquet porttitor lacus luctus accumsan tortor posuere ac. Morbi quis commodo odio aenean sed adipiscing diam. Vitae purus faucibus ornare suspendisse sed. Vitae congue mauris rhoncus aenean vel. Ut ornare lectus sit amet est placerat in. Congue quisque egestas diam in.</p>
+          <button style="padding-bottom: 5px;" type="button" class="btn btn-outline-success" id="btn2">See more</button>
+        </div>
+        <div class="small-container-right">
+          <img src="src\assets\placeholder-image.png" class="title-image">
+        </div>
+      </div>
+
+      <!-- Experience #3 -->
+      <div class="flex-container-reverse" style="padding-bottom: 50px; padding-top: 40px;">
+        <div class="big-container-left">
+          <h3>Mauris</h3>
+          <p class="time-text">June 2022 - TBD</p> <!--Add your own dates-->
+          <p>Consequat id porta nibh venenatis cras sed felis eget. Viverra accumsan in nisl nisi. Quam id leo in vitae. Amet consectetur adipiscing elit pellentesque habitant morbi tristique senectus.</p>
+          <button style="padding-bottom: 5px;" type="button" class="btn btn-outline-success" id="btn3">Visit</button>
+        </div>
+        <div class="small-container-right">
+          <img src="src\assets\placeholder-image.png" class="title-image">
         </div>
       </div>
 
       <!-- Completed projects -->
       <h2 style="padding-bottom: 20px">My Completed Projects (recent to old)</h2>
-      </center>
+
+      <h2 style="padding-bottom: 20px">Resources</h2>
+
+      <!-- Contact me / ending section -->
+      <h2 style="padding-bottom: 20px; padding-top: 80px;">Contact Me</h2>
+
+      <img src="src\assets\placeholder-image.png"  class="rounded-corners" style="">
+
+      <br> <br>
+
+      <p style="padding-bottom: 20px;">I'd love to get in contact with you, so please feel free to reach me on any of these platforms. </p>
+
+      <a href="mailto:sidelkins77@gmail.com" class="fa fa-paper-plane" style="margin-right: 10px"></a>
+      <a href="https://www.instagram.com/sidelkins/" target="_blank" rel="noopener noreferrer" class="fa fa-instagram" style="margin-right: 10px"></a>
+      <a href="https://github.com/sidelkins" target="_blank" rel="noopener noreferrer" class="fa fa-github"></a>
+
+      <br> <br> <br>
+
 </div>
